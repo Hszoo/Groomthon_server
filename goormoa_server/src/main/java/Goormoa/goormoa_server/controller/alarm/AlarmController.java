043e5041f01e8f909e9381c6ba950d89d59bd341
김멍몽 <wjdahrrla@gmail.com>
2023-11-17 20:45:07 +0900

    [STYLE] log 출력 코드 추가

--- goormoa_server/src/main/java/Goormoa/goormoa_server/controller/alarm/AlarmController.java
+++ goormoa_server/src/main/java/Goormoa/goormoa_server/controller/alarm/AlarmController.java
@@ -1,9 +1,12 @@
 package Goormoa.goormoa_server.controller.alarm;
 
+import Goormoa.goormoa_server.controller.profile.ProfileController;
 import Goormoa.goormoa_server.dto.alarm.AlarmDTO;
 import Goormoa.goormoa_server.service.alarm.AlarmService;
 import Goormoa.goormoa_server.service.auth.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +18,11 @@ import java.util.List;
 public class AlarmController {
     private final AuthenticationService authenticationService;
     private final AlarmService alarmService;
+    private static final Logger logger = LoggerFactory.getLogger(AlarmController.class);
 
     @GetMapping("/alarmList") // 알람 목록 가져오기
     public ResponseEntity<List<AlarmDTO>> getAlarm() {
+        logger.info("알람 컨트롤러 -> 알람 목록 조회");
         String currentUserEmail = authenticationService.getCurrentAuthenticatedUserEmail();
         return ResponseEntity.ok(alarmService.getAlarms(currentUserEmail));
     }
--- goormoa_server/src/main/java/Goormoa/goormoa_server/controller/group/GroupController.java
+++ goormoa_server/src/main/java/Goormoa/goormoa_server/controller/group/GroupController.java
@@ -1,6 +1,7 @@
 package Goormoa.goormoa_server.controller.group;
 
 
+import Goormoa.goormoa_server.controller.follow.FollowController;
 import Goormoa.goormoa_server.dto.group.GroupDTO;
 import Goormoa.goormoa_server.dto.group.DividedGroups;
 import Goormoa.goormoa_server.dto.group.GroupDetailDTO;
@@ -10,6 +11,8 @@ import Goormoa.goormoa_server.entity.user.User;
 import Goormoa.goormoa_server.repository.user.UserRepository;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,22 +26,27 @@ public class GroupController {
     private final AuthenticationService authenticationService;
     private final GroupService groupService;
     private final UserRepository userRepository;
+    private static final Logger logger = LoggerFactory.getLogger(GroupController.class);
+
     /* 모집 중인 모임 조회 */
     @GetMapping
     public List<GroupDTO> getAllRecruitingGroups() {
+        logger.info("모임 컨트롤러 -> 모집 중인 모임 조회");
         String currentUserEmail = authenticationService.getCurrentAuthenticatedUserEmail();
         return groupService.getAllGroups(currentUserEmail);
     }
 
-    /* 사용자가 포함된 모든 모임 조회 */
+    /* 현재 사용자가 포함된 모든 모임 조회 */
     @GetMapping("/myGroups")
     public DividedGroups getMyGroupList() {
+        logger.info("모임 컨트롤러 -> 현재 사용자 모든 모임 조회");
         String currentUserEmail = authenticationService.getCurrentAuthenticatedUserEmail();
         return groupService.getMyGroups(currentUserEmail);
     }
-    /* 사용자가 포함된 모든 모임 조회 */
+    /* 특정 사용자가 포함된 모든 모임 조회 */
     @GetMapping("/myGroups/{userId}")
     public DividedGroups getOtherUserGroupList(@PathVariable Long userId) {
+        logger.info("모임 컨트롤러 -> 특정 사용자 모든 모임 조회");
         Optional<User> optionalUser = userRepository.findByUserId(userId);
         if(!optionalUser.isPresent()) {
             return null;
@@ -47,8 +55,10 @@ public class GroupController {
         return groupService.getMyGroups(user.getUserEmail());
     }
 
+    // 생성 처리
     @PostMapping
     public ResponseEntity<String> groupCreate(@RequestBody GroupDTO groupDTO) {
+        logger.info("모임 컨트롤러 -> 모임 생성");
         String currentUserEmail = authenticationService.getCurrentAuthenticatedUserEmail();
         String signupResult = groupService.save(currentUserEmail, groupDTO);
         return "error".equals(signupResult) ?
@@ -59,6 +69,7 @@ public class GroupController {
     // 수정 처리 : 모임의 host와 사용자가 같을 때만 가능
     @PostMapping("/update")
     public ResponseEntity<String> groupUpdate(@RequestBody GroupDTO groupDTO) {
+        logger.info("모임 컨트롤러 -> 모임 수정 요청");
         String currentUserEmail = authenticationService.getCurrentAuthenticatedUserEmail();
         String updateResult = groupService.update(currentUserEmail, groupDTO);
         return "error".equals(updateResult) ?
@@ -69,6 +80,7 @@ public class GroupController {
     // 삭제 처리 : 모임의 host와 사용자가 같을 때만 가능
     @DeleteMapping("/delete")
     public ResponseEntity<String> groupDelete(@RequestBody GroupDTO groupDTO) {
+        logger.info("그룹 컨트롤러 -> 모임 삭제 요청");
         String currentUserEmail = authenticationService.getCurrentAuthenticatedUserEmail();
         String deleteResult = groupService.delete(currentUserEmail, groupDTO);
         return "error".equals(deleteResult) ?
@@ -79,12 +91,14 @@ public class GroupController {
     /* 모임 상세 페이지 GET 요청 (완료)*/
     @GetMapping("/{groupId}")
     public GroupDetailDTO getGroup(@PathVariable Long groupId) {
+        logger.info("그룹 컨트롤러 -> 모임 상세 페이지 요청");
         return groupService.detailGroup(groupId);
     }
 
     /* 모임 모집 마감 요청 */
     @PostMapping("/complete")
     public void groupClose(@RequestBody GroupDTO groupDTO) {
+        logger.info("그룹 컨트롤러 -> 모임 모집 마감 요청");
         String currentUserEmail = authenticationService.getCurrentAuthenticatedUserEmail();
         groupService.closeGroup(currentUserEmail, groupDTO);
     }
@@ -92,18 +106,21 @@ public class GroupController {
     /* 모임 신청 요청 */
     @PostMapping("/{groupId}/apply")
     public String applyToGroup(@PathVariable Long groupId) {
+        logger.info("그룹 컨트롤러 -> 모임 신청 요청");
         String currentUserEmail = authenticationService.getCurrentAuthenticatedUserEmail();
         return groupService.applyToGroup(groupId, currentUserEmail);
     }
     /* 모임 신청 승인 */
     @PostMapping("/{groupId}/approve/{userId}")
     public String approveUserToGroup(@PathVariable Long groupId, @PathVariable Long userId) {
+        logger.info("그룹 컨트롤러 -> 모임 신청 승인");
         return groupService.approveApplication(groupId, userId);
     }
 
     /* 모임 신청 거부 */
     @PostMapping("/{groupId}/reject/{userId}")
     public String rejectUserToGroup(@PathVariable Long groupId, @PathVariable Long userId) {
+        logger.info("그룹 컨트롤러 -> 모임 신청 거부");
         return groupService.rejectApplication(groupId, userId);
     }
 
